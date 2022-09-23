93ce786cb7061b07d6e5b2c67f16932cb0816b3f
BerryRahul <113147217+BerryRahul@users.noreply.github.com>
2022-09-23 14:10:44 -0400

    Completed all the TODO's and completed the self-check to see if the code works.

--- src/main/java/Silly.java
+++ src/main/java/Silly.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.Random;
 
 /**
  * This file contains a few exercises to familiarize you with specific
@@ -80,6 +81,9 @@ public class Silly implements Comparable<Silly>{
      *       Make sure you document this method!
      */
 
+    public Silly(String string1, String string2) {
+        this.name = string1 + string2;
+    }
 
 
 
@@ -116,7 +120,7 @@ public class Silly implements Comparable<Silly>{
         y.countStatic();
         x.countStatic();
         x.countStatic();
-        int[] expected_values = {};
+        int[] expected_values = {0, 1, 2, 3};
 
         System.out.println("The countStatic calls will return " + Arrays.toString(expected_values));
     }
@@ -134,6 +138,7 @@ public class Silly implements Comparable<Silly>{
     @Override
     public String toString(){
         // TODO (Task 3): Implement the body of this method!
+        return this.name;
     }
 
     /**
@@ -158,7 +163,7 @@ public class Silly implements Comparable<Silly>{
         }
 
         Silly other = (Silly) o; // To access .name of o, we need to cast it.
-
+        return this.name.equals(other.name);
         // Hint: to compare strings, we need to use .equals()
         //       e.g. s1.equals(s2)
     }
@@ -194,16 +199,23 @@ public class Silly implements Comparable<Silly>{
          *                You can get the length of a string by using the
          *                .length() method.
          */
-    }
+        if (this.name.length() > other.name.length()) {
+            return 1;
+        }
+        if (this.name.length() < other.name.length()) {
+            return -1;
+        } else {
+            return 0;
+        }
 
-    /*
-     * TODO (Task 6): Submit the changes you made on GitHub!
-     *                When you submit it, go to the 'Actions' tab. You should
-     *                be able to see this exercise being autograded!
-     *
-     *                You can click on the ongoing action to see more details.
-     *                If the tests don't pass, look at the results and revise
-     *                accordingly.
-     */
-}
+        /*
+         * TODO (Task 6): Submit the changes you made on GitHub!
+         *                When you submit it, go to the 'Actions' tab. You should
+         *                be able to see this exercise being autograded!
+         *
+         *                You can click on the ongoing action to see more details.
+         *                If the tests don't pass, look at the results and revise
+         *                accordingly.
+         */
+    }}
 
